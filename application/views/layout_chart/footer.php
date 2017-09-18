441e21252a7d57015685622615777837f399c8c9
Rizky Nurachmad <30620983+rizkynurs@users.noreply.github.com>
2017-09-18 13:50:11 +0700

    coba

---
+++ application/views/layout_chart/footer.php
@@ -0,0 +1,24 @@
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=base_url();?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=base_url();?>assets/vendor/metisMenu/metisMenu.min.js"></script>
+    
+     <!--Custom Theme JavaScript -->
+    <script src="<?=base_url();?>assets/dist/js/sb-admin-2.js"></script>
+    <script src="<?=base_url();?>assets/dist/js/scroll.js"></script>
+
+    <script src="<?=base_url();?>assets/vendor/Chart/Chart.bundle.js"></script>
+    <script src="<?=base_url();?>assets/vendor/Chart/Chart.bundle.min.js"></script>
+    <script src="<?=base_url();?>assets/vendor/Chart/Chart.js"></script>
+    <script src="<?=base_url();?>assets/vendor/Chart/Chart.min.js"></script>  
+    
+    <!--<script src="../dist/js/chart.min.js"></script>-->
+
+
+</body>
+
+</html>
---
+++ application/views/pages/charts/event-guards.php
@@ -0,0 +1,141 @@
+<div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Event Guards </h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>            
+            <!-- /.row -->
+            <div class="row">     
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Guards Daily Booking Chart
+                        </div>
+                        <!-- /.panel-heading -->    
+                        <div class="panel-body">
+                            <div class="flot-chart">        
+                                <canvas id="egbookdaily" width="400" height="300"></canvas>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>       
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Guards Weekly Booking Chart
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egbookweekly" width="400" height="300"></canvas>
+                            </div>                            
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+                
+                <div class="col-lg-6">                
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Guards Monthly Booking Chart
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egbookmonthly" width="400" height="300"></canvas>
+                            </div>                            
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Guards Yearly Booking Chart
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egbookyearly" width="400" height="300"></canvas>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Gurads Daily Income Charts
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egincomedaily" width="400" height="300"></canvas>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Gurads Weekly Income Charts
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egincomeweekly" width="400" height="300"></canvas>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Gurads Monthly Income Charts
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egincomemonthly" width="400" height="300"></canvas>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>  
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Event Gurads Yearly Income Charts
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <canvas id="egincomeyearly" width="400" height="300"></canvas>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>                         
+            </div>
+            <!-- /.row -->
+            <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+    <script src="<?=base_url();?>assets/data/chart-data-eg.js"></script>
