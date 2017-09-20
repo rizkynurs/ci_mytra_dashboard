d601bc2c9aa8aeca3b01e8e681fa6e5b66c2a6c8
Rizky Nurachmad <30620983+rizkynurs@users.noreply.github.com>
2017-09-20 10:20:31 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:8080/ci_mytra_dashboard';
+
+$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/'; 
+//$config['base_url'] = 'http://localhost:8080/ci_mytra_dashboard';
 
 /*
 |--------------------------------------------------------------------------
