94cdb3c5cf5178d5712ac8eaa925bde6443f1a13
Rizky Nurachmad <30620983+rizkynurs@users.noreply.github.com>
2017-10-12 11:20:23 +0700

    + md5

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -9,10 +9,11 @@ class Login extends CI_Controller {
 		$valid = $this->form_validation;
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
+		$passwordx = md5($password);
 		$valid->set_rules('username','Username','required');
 		$valid->set_rules('password','Password','required');
 		if($valid->run()) {
-			$this->simple_login->login($username,$password, base_url('dasboard'), base_url('login'));
+			$this->simple_login->login($username,$passwordx, base_url('dasboard'), base_url('login'));
 		}
 		// End fungsi login
 		$data = array(	'title'	=> 'Halaman Login Administrator');
